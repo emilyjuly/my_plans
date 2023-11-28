95c78dc83641eb6b58c7f8e25eba1842d668cde5
July Gomes Domingos <ejuly@minsait.com>
2023-11-27 21:22:13 -0300

    ⛓️ Fix: Alterando configuração de CORS

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  app.enableCors();
+  app.enableCors({ methods: 'GET,HEAD,PUT,PATCH,POST,DELETE' });
 
   const config = new DocumentBuilder()
     .setTitle('My Plans')
